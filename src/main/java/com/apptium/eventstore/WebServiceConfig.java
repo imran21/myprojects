ab3b09e9450dde45ac0f7314fa5b283f3e437ba1
taftwallaceiii <taftwallaceiii@bonitohome>
2018-02-11 14:36:27 -0500

    Soap Event Store

---
+++ src/main/java/com/apptium/eventstore/WebServiceConfig.java
@@ -0,0 +1,66 @@
+package com.apptium.eventstore;
+
+import java.util.Properties;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.soap.server.endpoint.SoapFaultDefinition;
+import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+import com.apptium.eventstore.util.DetailSoapFaultDefinitionExceptionResolver;
+import com.apptium.eventstore.util.ServiceFaultException;
+
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+	
+	 @Bean
+	    public SoapFaultMappingExceptionResolver exceptionResolver(){
+	        SoapFaultMappingExceptionResolver exceptionResolver = new DetailSoapFaultDefinitionExceptionResolver();
+
+	        SoapFaultDefinition faultDefinition = new SoapFaultDefinition();
+	        faultDefinition.setFaultCode(SoapFaultDefinition.SERVER);
+	        exceptionResolver.setDefaultFault(faultDefinition);
+
+	        Properties errorMappings = new Properties();
+	        errorMappings.setProperty(Exception.class.getName(), SoapFaultDefinition.SERVER.toString());
+	        errorMappings.setProperty(ServiceFaultException.class.getName(), SoapFaultDefinition.SERVER.toString());
+	        exceptionResolver.setExceptionMappings(errorMappings);
+	        exceptionResolver.setOrder(1);
+	        return exceptionResolver;
+	    }
+	
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(servlet, "/ws/*");
+	}
+
+	@Bean(name = "events")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema eventsSchema) {
+		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+		wsdl11Definition.setPortTypeName("EventsPort");
+		wsdl11Definition.setLocationUri("/ws");
+		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+		wsdl11Definition.setSchema(eventsSchema);
+		return wsdl11Definition;
+	}
+
+	@Bean
+	public XsdSchema eventsSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("soapEvent.xsd"));
+	}
+
+}
