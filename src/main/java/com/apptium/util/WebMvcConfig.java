745351e3c1f9ebbe7427db2810c8b17e0f9a3f3e
Nirmal Srinath <Nirmal Srinath nsrinath@apptium.com>
2018-10-29 18:48:04 +0530

    add file exception log changes

---
+++ src/main/java/com/apptium/util/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.apptium.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+ 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) { 
+        registry.addInterceptor(new FlowidInteceptor());
+        registry.addInterceptor(new MDC_Inteceptor());
+ 
+    }
+
+}
